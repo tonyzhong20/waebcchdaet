59effc06268c417e1b7ddb785b51ed163df2b527
joli <joli@ebay.com>
2014-07-03 16:34:49 -0700

    updates

--- models/service/image.js
+++ models/service/image.js
@@ -1,12 +1,14 @@
 exports.buildModel = function (returnObj, result)
 {	
-    returnObj.view = 'image';
+    console.dir(result);
+	
+	returnObj.view = 'image';
 
     returnObj.data.msgType = 'image';
     
-    returnObj.data.picUrl = 'echo:' + result.xml.PicUrl[0];
+    returnObj.data.picUrl = result.xml.PicUrl[0];
  
-    returnObj.data.mediaID = 'echo:'+ result.xml.MediaID[0];
+    returnObj.data.mediaID = result.xml.MediaID[0];
     
     console.dir(result);
 }
