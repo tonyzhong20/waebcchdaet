aa20a3684d82616a00a078804499cdd19fd99303
joli <joli@ebay.com>
2014-07-08 16:10:53 -0700

    updates

--- models/service/event.js
+++ models/service/event.js
@@ -2,9 +2,11 @@ exports.buildModel = function (returnObj, result)
 {	
 	returnObj.view = 'event';
 
-    returnObj.data.msgType = 'event';
+    returnObj.data.msgType = 'text';
  
     returnObj.data.event = result.xml.Event[0];
-       
+     
+    returnObj.data.content = 'Thanks for subscribing!'+ result.xml.Content;
+    
     //result.xml.Event[0] = 'Hi. Thanks for following the official account!';
 }
