4a8cf669dce5ae9f4b9a9caf2be39ccb83f8ca70
joli <joli@ebay.com>
2014-07-08 15:49:16 -0700

    updates

--- models/service/event.js
+++ models/service/event.js
@@ -6,5 +6,10 @@ exports.buildModel = function (returnObj, result)
  
     returnObj.data.event = result.xml.Event[0];
     
+    if (returnObj.data.event = 'subscribe') {
+    	console.log('Thanks for subscribing!');
+    }
+    else console.log('Please subscribe!');
+    
     //result.xml.Event[0] = 'Hi. Thanks for following the official account!';
 }
