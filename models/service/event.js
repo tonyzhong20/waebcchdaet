6b5952d1b1ceac4c0ccd3b32c29e7dc649be512d
joli <joli@ebay.com>
2014-07-08 15:08:15 -0700

    updates

--- models/service/event.js
+++ models/service/event.js
@@ -4,9 +4,7 @@ exports.buildModel = function (returnObj, result)
 
     returnObj.data.msgType = 'event';
 
-    returnObj.data.event = 'Thanks for following the official account!';
+    //returnObj.data.event = 'Thanks for following the official account!';
     
-    //returnObj.data.event = result.xml.Event;
-
-    //result.xml.Event = 'Hi. Thanks for following the official account!';
+    returnObj.data.event = result.xml.Event + 'Hi. Thanks for following the official account!';
 }
