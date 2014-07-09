c75773d18bd84825dbb0d4568051ec72045b2823
joli <joli@ebay.com>
2014-07-09 11:59:10 -0700

    updates

--- models/service/news.js
+++ models/service/news.js
@@ -1,6 +1,17 @@
 exports.buildModel = function (returnObj, result)
 {	
-	returnObj.view = 'news';
+	returnObj.view = 'text';
 
     returnObj.data.msgType = 'news';
+    
+    if (result.xml.Content == 'news')
+    {
+    	returnObj.view = 'news';
+    }
+    
+    //returnObj.data.articleCount
+    
+    //returnObj.data.articles
+    
+    
 }
